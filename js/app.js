e3f9ff8226babac9ee6b338b355ca8664a49e753
Richard Gordon <richgordon95@gmail.com>
2018-04-05 17:23:46 -0400

    added pseudocode for updating location of enemies

--- js/app.js
+++ js/app.js
@@ -1,21 +1,32 @@
+
+// Place all enemy objects in an array called allEnemies
+var allEnemies = [ ];
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+		this.loc = (x, y);
+		this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+		//push enemy to allEnemies array 
+		allEnemies.push(this);
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    //var loc = ;//????
+    // this.loc *moves at* this.speed
 		// You should multiply any movement by the dt parameter
 		//??? loc *= dt
 		// which will ensure the game runs at the same speed for
     // all computers.
+			//check if encountered enemy
+//		if (this loc = allEnemies[i].loc) {
+			//game over
+	//	}
 };
 
 // Draw the enemy on the screen, required method for game
@@ -35,26 +46,31 @@ var Player = function() {
     this.sprite = 'images/char-boy.png';
 };
 
-// Update the enemy's position, required method for game
+// Update the player's position, required method for game
 // Parameter: dt, a time delta between ticks
 Player.prototype.update = function(dt) {
-    //var loc = ;//????
+    //var loc = (0, 2);//????
 		// You should multiply any movement by the dt parameter
-		//??? loc *= dt
+		//loc = (loc + ***input*** ) * dt);
 		// which will ensure the game runs at the same speed for
     // all computers.
 };
 
-// Draw the enemy on the screen, required method for game
+// Draw the player on the screen, required method for game
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
+
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
+//enemy (x, y, speed)
+var enemy1 = new Enemy (1, 1, 3);
+var enemy2 = new Enemy (2, 1, 5);
+var enemy3 = new Enemy (3, 1, 2);
+var enemy4 = new Enemy (4, 2, 4);
 // Place the player object in a variable called player
-
-
+var player = new Player ();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
