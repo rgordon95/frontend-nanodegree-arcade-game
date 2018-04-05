350d6f0f232734ca1c8f15ecd19a32ef6a02468e
Richard Gordon <richgordon95@gmail.com>
2018-04-05 16:54:54 -0400

    create player class

--- js/app.js
+++ js/app.js
@@ -11,8 +11,10 @@ var Enemy = function() {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
+    //var loc = ;//????
+		// You should multiply any movement by the dt parameter
+		//??? loc *= dt
+		// which will ensure the game runs at the same speed for
     // all computers.
 };
 
@@ -24,7 +26,29 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
 
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-boy.png';
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function(dt) {
+    //var loc = ;//????
+		// You should multiply any movement by the dt parameter
+		//??? loc *= dt
+		// which will ensure the game runs at the same speed for
+    // all computers.
+};
+
+// Draw the enemy on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
